2418ce7e55c73d9a0738ab8c73051cc9ae9a766b
Skorokhodov21 <90981434+Skorokhodov21@users.noreply.github.com>
2023-02-08 02:03:57 +0300

    Add files via upload

---
+++ PhoneMDK/PhoneMDK/PhoneDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneMDK
+{
+    public class PhoneDemo
+    {
+        public static void Main()
+        {
+            Phone phone1 = new Phone("9898482928", "iPhone", 8.0);
+            Phone phone2 = new Phone("938123132", "Honor", 15.0);
+            Phone phone3 = new Phone();
+
+            Console.WriteLine(phone1.getNumber());
+            Console.WriteLine(phone1.model);
+            Console.WriteLine(phone1.weight);
+            Console.WriteLine();
+
+            Console.WriteLine(phone2.getNumber());
+            Console.WriteLine(phone2.model);
+            Console.WriteLine(phone2.weight);
+            Console.WriteLine();
+
+            Console.WriteLine(phone3.getNumber());
+            Console.WriteLine(phone3.model);
+            Console.WriteLine(phone3.weight);
+
+            phone1.receiveCall("Мама");
+            phone2.receiveCall("Папа");
+
+        }
+    }
+}
---
+++ PhoneMDK/PhoneMDK/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PhoneMDK
+{
+    public class Phone
+    {
+        public string model { get; set; }
+        public string number { get; set; }
+        public double weight { get; set; }
+
+        public Phone(string n, string m, double w)
+        {
+            number = n;
+            model = m;
+            weight = w;
+        }
+
+        public Phone(string n, string m)
+        {
+            model = m;
+            number = n;
+        }
+
+        public Phone() { }
+
+        public void receiveCall(string name)
+        {
+            Console.WriteLine("Calling " +  name);
+        }
+
+        public string getNumber()
+        {
+            return number;
+        }
+
+    }
+}
